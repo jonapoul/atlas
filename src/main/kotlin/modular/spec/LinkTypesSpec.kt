cf757fd0ec95f92711418a8d567d9b63878183d2
Jon Poulton <jpoulton@pm.me>
2025-09-13 17:50:05 +0100

    Update LinkTypesSpec.kt

--- src/main/kotlin/modular/spec/LinkTypesSpec.kt
+++ src/main/kotlin/modular/spec/LinkTypesSpec.kt
@@ -9,8 +9,8 @@ import modular.gradle.ModularDsl
 import org.gradle.api.provider.SetProperty
 import org.gradle.internal.impldep.org.intellij.lang.annotations.Language
 import kotlin.text.RegexOption.IGNORE_CASE
-import org.gradle.internal.impldep.kotlinx.serialization.Serializable as KSerializable
 import java.io.Serializable as JSerializable
+import org.gradle.internal.impldep.kotlinx.serialization.Serializable as KSerializable
 
 /**
  * Used to configure expected link "types" between your modules. The majority of the time, these are only ever
