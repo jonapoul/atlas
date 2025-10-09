36221b79b7b75ff0abe0bb07df540e8d52941aae
Jon Poulton <jpoulton@pm.me>
2025-10-09 21:30:41 +0100

    Test multiple plugins

---
+++ modular-test/src/test/kotlin/modular/MultiplePluginsTest.kt
@@ -0,0 +1,23 @@
+package modular
+
+import assertk.assertThat
+import assertk.assertions.contains
+import modular.test.ScenarioTest
+import modular.test.runTask
+import modular.test.scenarios.MultiplePluginsApplied
+import kotlin.test.Test
+
+class MultiplePluginsTest : ScenarioTest() {
+  @Test
+  fun `Fail when multiple modular plugins are applied`() = runScenario(MultiplePluginsApplied) {
+    // when
+    val result = runTask("modularGenerate").buildAndFail()
+
+    // then
+    assertThat(result.output).contains(
+      """
+        > Cannot add extension with name 'modular', as there is an extension already registered with that name.
+      """.trimIndent()
+    )
+  }
+}
