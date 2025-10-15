bcb84475ca5de138cc50d7d15c5f3146900fc247
Jon Poulton <jpoulton@pm.me>
2025-10-15 21:13:05 +0100

    d

--- atlas-test/src/test/kotlin/atlas/ConfigureOnDemandTest.kt
+++ atlas-test/src/test/kotlin/atlas/ConfigureOnDemandTest.kt
@@ -5,29 +5,26 @@
 package atlas
 
 import assertk.assertThat
-import assertk.assertions.exists
 import atlas.test.ScenarioTest
 import atlas.test.allTasksSuccessful
 import atlas.test.noTasksFailed
 import atlas.test.runTask
 import atlas.test.scenarios.D2ConfigureOnDemand
+import atlas.test.scenarios.GraphvizConfigureOnDemand
+import atlas.test.scenarios.MermaidConfigureOnDemand
+import java.io.File
 import kotlin.test.Test
 
 internal class ConfigureOnDemandTest : ScenarioTest() {
-  @Test
-  fun `Support Gradle configureOnDemand flag`() = runScenario(D2ConfigureOnDemand) {
-    // when we generate
+  @Test fun `D2 configureOnDemand`() = runScenario(D2ConfigureOnDemand) { testCase() }
+  @Test fun `Graphviz configureOnDemand`() = runScenario(GraphvizConfigureOnDemand) { testCase() }
+  @Test fun `Mermaid configureOnDemand`() = runScenario(MermaidConfigureOnDemand) { testCase() }
+
+  private fun File.testCase() {
     val generate = runTask("atlasGenerate").build()
-
-    // then
     assertThat(generate).allTasksSuccessful()
-    assertThat(resolve("a/atlas/chart.d2")).exists()
-    assertThat(resolve("a/atlas/chart.svg")).exists()
 
-    // when we check
     val check = runTask("atlasCheck").build()
-
-    // then
     assertThat(check).noTasksFailed()
   }
 }
